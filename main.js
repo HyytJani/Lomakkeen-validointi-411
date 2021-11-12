54b500feaa4c62b13c344893a0d8dd4333696751
HyytJani <jhi.hyytiainen@gmail.com>
2021-11-12 11:08:49 +0200

    korjaus

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ function validateForm() {
       alert("Käyttäjä ID täytyy olla vähintään 6 merkkiä pitkä ");
       return false;
     }
-    let salasana = document.forms["myForm"]["salasana"].value;
-    if (salasana.length<1) {
-      alert("Syötä salasana");
-      return false;
-    }
-
     let numero= document.forms["myForm"]["postinumero"].value;
     if (numero.length<5) {
       alert("Postinumerossa täytyy olla 5 numeroa");
@@ -19,19 +13,11 @@ function validateForm() {
       alert("Postinumerossa täytyy olla 5 numeroa");
       return false;
     }
-    let email= document.forms["myForm"]["email"].value; 
-    if (email.length<1){
-      alert("Syötä sähköpostiosoite")
-      return false;
-    }
     if (myForm.suomi.checked == false && myForm.muu.checked == false )      
       {
         alert ('Valitse kieli.');
         return false;
 
       }
-    let maa=document.forms["myForm"]["maa"].value;
-      if (maa == "(Valitse maa)") {
-        alert("Please select a selection");
-        return false;}
+
 }
